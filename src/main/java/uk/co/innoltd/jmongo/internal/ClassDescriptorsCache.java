834d460250cbbd3ef6cc970d1f304e7914d2c4eb
Christophe Popov <chrpopov@gmail.com>
2012-03-06 21:36:09 +0000

    upgraded lib versions

--- src/main/java/uk/co/innoltd/jmongo/internal/ClassDescriptorsCache.java
+++ src/main/java/uk/co/innoltd/jmongo/internal/ClassDescriptorsCache.java
@@ -1,25 +1,25 @@
 package uk.co.innoltd.jmongo.internal;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ClassDescriptorsCache {
 	private final Map<String, ClassDescriptor> descriptors;
 
 	public ClassDescriptorsCache() {
-		descriptors = new HashMap<String, ClassDescriptor>();
+		descriptors = new ConcurrentHashMap<String, ClassDescriptor>();
 	}
 
 	public ClassDescriptor get(Class<?> clazz) {
 		ClassDescriptor descriptor = descriptors.get(clazz.getCanonicalName());
-		if (descriptor==null) {
+		if (descriptor == null) {
 			descriptor = ClassDescriptor.get(clazz);
-			// TODO see if there is any danger here when multithreaded access.			
+			// TODO see if there is any danger here when multithreaded access.
 			descriptors.put(clazz.getCanonicalName(), descriptor);
 		}
 		return descriptor;
 	}
-	
+
 	int getSize() {
 		return descriptors.size();
 	}
